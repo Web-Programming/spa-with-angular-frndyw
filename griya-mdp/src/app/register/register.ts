1275e7c3688c9a1f42359c4f0e8a747ba4bfd82e
mfrendi <mfrendiriyannanda_2327240161@mhs.mdp.ac.id>
2025-11-24 16:58:17 +0700

    committ

--- griya-mdp/src/app/register/register.ts
+++ griya-mdp/src/app/register/register.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Formgroup, ReactiveFormModule, validators, FormBuilder } from '@angular/form';
+import { Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, ReactiveFormModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBUilder)
+  this.registerForm = this.fb.group({
+    name: [ '', [ Validators,Required, Validators.minLength(2)]]
+  })
 }
